90c33865d0dfa9b730fd431e8078bd85312bca58
Re Tyre <grexer@gmail.com>
2025-01-18 21:53:51 -0500

    Update BoardConfig.mk
    Signed-off-by: Re Tyre <grexer@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/samsung/gta9p
+DEVICE_PATH := device/samsung/gta9pwifi
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
