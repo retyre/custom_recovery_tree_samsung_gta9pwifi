cfb177e26dbd88d4c9815f2d844ff5fd5e58dde5
Re Tyre <grexer@gmail.com>
2025-01-18 22:01:43 -0500

    Update vendorsetup.sh
    Signed-off-by: Re Tyre <grexer@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -18,7 +18,7 @@
 # 	Please maintain this if you use this script or any part of it
 #
 
-FDEVICE="gta9p"
+FDEVICE="gta9pwifi"
 #set -o xtrace
 
 fox_get_target_device() {
@@ -49,7 +49,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 # Device Info
     export FOX_ARCH=arm64
     export FOX_VARIANT="12.1"
-    export TARGET_DEVICE_ALT="gta9p"
+    export TARGET_DEVICE_ALT="gta9pwifi"
   
 # Funtions
     export FOX_REPLACE_BUSYBOX_PS=1
